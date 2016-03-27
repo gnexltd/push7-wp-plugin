02bdecfb01c3f010c035eb49b00881932d70d151
nkpoid <nkpoidjp@gmail.com>
2016-03-28 00:02:12 +0900

    ver 1.4.0 bugfix, カスタム投稿タイプ別及びカテゴリ別の設定に対応しました.

--- metabox.php
+++ metabox.php
@@ -1,5 +1,5 @@
 <span>通知を</span>
 <br>
-<input type="radio" name="push7_is_notify" value="true" <?php checked("true", self::check_postType()); ?>>する
+<input type="radio" name="push7_is_notify" value="true" <?php checked("true", self::check_push()); ?>>する
 <br>
-<input type="radio" name="push7_is_notify" value="false" <?php checked("false", self::check_postType()); ?>>しない
+<input type="radio" name="push7_is_notify" value="false" <?php checked("false", self::check_push()); ?>>しない
--- push7.php
+++ push7.php
@@ -126,16 +126,26 @@ class Push7 {
     register_setting('push7-settings-group', 'push7_apikey');
     register_setting('push7-settings-group', 'push7_sslverify_disabled');
 
-    // デフォルトの投稿タイプ(post)及びカスタム投稿タイプのプッシュ通知設定
-    foreach (get_post_types(array('_builtin' => false)) + array('post' => 'post') as $post_type) {
-      $cpt_new = "push7_push_".$post_type."_on_new";
-      $cpt_update = "push7_push_".$post_type."_on_update";
-      register_setting('push7-settings-group', $cpt_new);
-      register_setting('push7-settings-group', $cpt_update);
-      if(!get_option($cpt_new)) update_option($cpt_new, "true");
-      if(!get_option($cpt_update)) update_option($cpt_update, "true");
+    // カテゴリ設定
+    foreach (get_categories(array('exclude' => '1')) as $category) {
+      $new = "push7_push_ctg_".$category->slug."_on_new";
+      $update = "push7_push_ctg_".$category->slug."_on_update";
+      register_setting('push7-settings-group', $new);
+      register_setting('push7-settings-group', $update);
+      if(!(get_option($new))) update_option($new, "false");
+      if(!get_option($update)) update_option($update, "false");
     }
-    // 初期値の設定
+
+    // デフォルトの投稿タイプ(post)及びカスタム投稿タイプ設定
+    foreach (self::post_types() as $post_type) {
+      $new = "push7_push_pt_".$post_type."_on_new";
+      $update = "push7_push_pt_".$post_type."_on_update";
+      register_setting('push7-settings-group', $new);
+      register_setting('push7-settings-group', $update);
+      if(!get_option($new)) update_option($new, "false");
+      if(!get_option($update)) update_option($update, "false");
+    }
+
     if(!get_option("push7_sslverify_disabled")) update_option("push7_sslverify_disabled", "false");
 
     load_plugin_textdomain( 'push7', null, dirname(__FILE__) . '/languages' );
@@ -153,8 +163,7 @@ class Push7 {
   }
 
   public function metabox() {
-    $post_types = get_post_types(array('_builtin' => false)) + array('post' => 'post');
-    foreach ($post_types as $post_type) {
+    foreach (self::post_types() as $post_type) {
       add_meta_box(
         'push7metabox',
         __( 'Push7 通知設定', 'push7' ),
@@ -173,24 +182,52 @@ class Push7 {
     include 'metabox.php';
   }
 
-  public static function check_postType(){
+  public static function get_push_config($type, $identity) {
     global $post;
-    $post_type = get_post_type($post);
+    $new = "push7_push_".$type."_".$identity."_on_new";
+    $update = "push7_push_".$type."_".$identity."_on_update";
     switch ($post->post_status) {
       // 新規投稿時
       case 'auto-draft':
-        return get_option("push7_push_".$post_type."_on_new");
+        return get_option($new);
       // 記事更新時
       case 'publish':
-        return get_option("push7_push_".$post_type."_on_update");
+        return get_option($update);
       case 'draft':
-        return get_option("push7_push_".$post_type."_on_update");
+        return get_option($update);
     }
+  }
+
+  public static function check_push(){
+    global $post;
+    $flag = true;
+    $categories = get_the_category($post);
+    foreach ($categories as $category) {
+      if (self::get_push_config("ctg", $category->slug) == "false") {
+        $flag = false;
+        break;
+      }
+    }
+    if (self::get_push_config("pt", get_post_type($post)) == "false") $flag = false;
+    return $flag ? "true" : "false";
   }
 
   public static function admin_url () {
     $args = array( 'page' => 'push7' );
     return add_query_arg( $args ,  admin_url( 'options-general.php' ));
+  }
+
+  public static function post_types() {
+    return get_post_types(array('_builtin' => false)) + array('post' => 'post');
+  }
+
+  public static function disp_post_type($post_type) {
+    switch ($post_type) {
+      case 'post':
+        return 'post(通常の投稿)';
+      default:
+        return $post_type;
+    }
   }
 
   public static function x_headers() {
