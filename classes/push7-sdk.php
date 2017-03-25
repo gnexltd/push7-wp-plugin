ad41cee3705d9cccab78ae99359600d36435a47b
Shun Kashiwa <shun.kashiwa@live.jp>
2017-03-25 11:58:37 +0900

    avoid deep nesting

--- classes/push7-sdk.php
+++ classes/push7-sdk.php
@@ -6,12 +6,13 @@ class Push7_Box {
   }
 
   public function render() {
-    $appno = get_option('push7_appno');
-    if (get_option('push7_sdk_enabled') === 'true' && $appno) {
-      ?>
-        <script src="https://sdk.push7.jp/v2/p7sdk.js"></script>
-        <script>p7.init("<?php echo $appno;?>");</script>
-      <?php
+    $appno = get_option('push7_appno', null);
+    if (get_option('push7_sdk_enabled') !== 'true' || empty($appno)){
+      return;
     }
+    ?>
+    <script src="https://sdk.push7.jp/v2/p7sdk.js"></script>
+    <script>p7.init("<?php echo $appno;?>");</script>
+    <?php
   }
 }
