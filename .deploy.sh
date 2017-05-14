fc73b2a23cb6e2fff4d65a35b43f37ef3800b5df
otofune <otofune@gmail.com>
2017-05-15 01:53:09 +0900

    travis: タグを切るようになった

--- .deploy.sh
+++ .deploy.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-rm -rf .git
+# バージョン取得
+CURRENT_VERSION=$(grep 'Stable tag:' readme.txt | cut -d' ' -f3)
+# .git削除のためコピー
+cp -r ./ ../push7-publishee
+# svn checkoutし作業する為に移動
 cd ../
+# subversionの中に.gitを保存したくないので削除
+rm -rf ./push7-publishee/.git
 svn checkout https://plugins.svn.wordpress.org/push7/
-cd push7/
-svn remove trunk
-cp -r ../push7-wp-plugin ./trunk
-svn add trunk
-svn commit -m "release for $TRAVIS_COMMIT" --username $WP_USER --password $WP_PASSWORD
+cd ./push7/
+# subversionに現在のタグリリースがない場合にリリースを行う
+if [ ! -d "tags/$CURRENT_VERSION" ]; then
+  # 現状のtrunk削除
+  svn remove ./trunk
+  cp -r ../push7-publishee ./trunk
+  svn add ./trunk
+  # タグを作成
+  cp -r ../push7-publishee "./tags/$CURRENT_VERSION"
+  svn add "./tags/$CURRENT_VERSION"
+  # コミットする
+  svn commit -m "release for $TRAVIS_COMMIT" --username $WP_USER --password $WP_PASSWORD
+  [ $? -eq 0 ] && echo "バージョン ($CURRENT_VERSION) がリリースされました"
+else
+  echo "このバージョン ($CURRENT_VERSION) はすでにリリースされています"
+fi
