c190e00abf4061f99ee79d5a754abc3e1cdda52c
otofune <otofune@gmail.com>
2017-05-16 02:11:48 +0900

    travis: アーリリターンとインデント波動拳
    アーリリターンによってインデントを浅くするように修正
    また、svnコミットの終了ステータスでdeployを終了するように

--- .deploy.sh
+++ .deploy.sh
@@ -11,24 +11,35 @@ fi
 CURRENT_VERSION=$(grep 'Stable tag:' readme.txt | cut -d' ' -f3)
 # .git削除のためコピー
 cp -r ./ ../push7-publishee
-# svn checkoutし作業する為に移動
+# 作業する為に移動
 cd ../
 # subversionの中に.gitを保存したくないので削除
 rm -rf ./push7-publishee/.git
+
+# svnリポジトリをcheckout
 svn checkout https://plugins.svn.wordpress.org/push7/
 cd ./push7/
-# subversionに現在のタグリリースがない場合にリリースを行う
-if [ ! -d "tags/$CURRENT_VERSION" ]; then
-  # 現状のtrunk削除
-  svn remove ./trunk
-  cp -r ../push7-publishee ./trunk
-  svn add ./trunk
-  # タグを作成
-  cp -r ../push7-publishee "./tags/$CURRENT_VERSION"
-  svn add "./tags/$CURRENT_VERSION"
-  # コミットする
-  svn commit -m "release for $TRAVIS_COMMIT" --username $WP_USER --password $WP_PASSWORD
-  [ $? -eq 0 ] && echo "バージョン $CURRENT_VERSION がリリースされました。"
-else
+
+# subversionに現在のタグリリースがある場合に終了
+if [ -d "tags/$CURRENT_VERSION" ]
+then
   echo "バージョン $CURRENT_VERSION はすでにリリースされています。"
+  exit 0
 fi
+
+# 現状のtrunk削除
+svn remove ./trunk
+cp -r ../push7-publishee ./trunk
+svn add ./trunk
+# タグを作成
+cp -r ../push7-publishee "./tags/$CURRENT_VERSION"
+svn add "./tags/$CURRENT_VERSION"
+# コミットする
+svn commit -m "release for $TRAVIS_COMMIT" --username $WP_USER --password $WP_PASSWORD
+
+# svnコミットのステータスをキャッシュ
+code=$?
+# 成功時は成功したことを表示
+[ $code -eq 0 ] && echo "バージョン $CURRENT_VERSION がリリースされました。" || echo "バージョン $CURRENT_VERSION のリリースが失敗しました。"
+# キャッシュしたステータスで終了
+exit $code
